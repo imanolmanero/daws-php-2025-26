9f06058bad5b0d2f33c91796bad692db96789856
imanol.manero <imanol.manero.51413@ikasle.egibide.org>
2025-10-06 12:32:06 +0200

    cosas

---
+++ POO/publicacion.php
@@ -0,0 +1,29 @@
+<?php 
+
+class publicacion {
+    //Propiedades
+    private string $autores;
+    private int $año;
+    private string $editorial;
+    private string $titulo;
+    private string $texto;
+
+    //Constructor
+    public function __construct($autores,$año,$editorial,$titulo,$texto) {
+        $this->autores = $autores;
+        $this->año = $año;
+        $this->editorial = $editorial;
+        $this->titulo = $titulo;
+        $this->texto = $texto;
+    }
+    
+    //Metodo -> leer
+    public function leer(){
+        return $this->texto;
+    }
+
+    
+
+
+}
+
